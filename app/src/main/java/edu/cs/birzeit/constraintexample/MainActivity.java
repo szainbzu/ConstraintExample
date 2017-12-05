50c39b8d6f90df432faf4ed6c599d81d485183e2
szainbzu <szain@birzeit.edu>
2017-12-05 10:36:12 +0200

    changed.....

--- app/src/main/java/edu/cs/birzeit/constraintexample/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/constraintexample/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test comment
         setContentView(R.layout.activity_main);
     }
 }
